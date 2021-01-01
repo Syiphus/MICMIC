a33fc644d87878f4942ae2a619a819966a00b10f
Diogo <diogomoreira31@hotmail.com>
2021-01-01 10:24:32 +0000

    Fixed Bugs and added Code Explanations

--- Project01_C/Project01_C/main.c
+++ Project01_C/Project01_C/main.c
@@ -3,82 +3,80 @@
 #define F_CPU 16000000UL
 #include <util/delay.h>
 
+/* In this first line we added a "function" that will compute the motor speed when we "feed" it with the percentage */
+#define DC(valor) ((255*valor)/100)
 
-#define DC(valor) ((255*valor)/100)		// velocidade do motor
+/*This table will be used to display the numbers on the screen, we added all the values 0-9 so it is easier to add more if needed
+  we also added the "-" for the negative sign*/
+const unsigned char display[] = {0xc0, 0xf9, 0xa4, 0xb0, 0x99, 0x92, 0x82, 0xf8, 0x80, 0x90, 0xbf, 0xff};
 
-// criar tabela em memória de programa - const
-const unsigned char display[] = {0xc0, 0xf9, 0xa4, 0xb0, 0x99, 0x92, 0x82, 0xf8, 0x80, 0x90, 0xbf, 0xff}; //0-9, "-", desligado
-
-// variáveis globais
+/*This is our global variables, all volatile as all of them are altered in the middle of the code*/
 volatile unsigned char switch_;
-volatile unsigned char flag;
+volatile unsigned char flag,negative=0;
 volatile unsigned char screen1, screen2, screen3;
 volatile unsigned char motor;
 
-
+/*This is the initialization function where all the starting commands will be performed (such as defining ports and interrupts)*/
 void inic(void)
 {
-	
-	DDRD = 0b11000000;		// configura porta - switch_
-	PORTD = 0b00111111;		// pinos 6,7 saídas, restantes entrada
-	
-	DDRC = 0b11111111;		// configura portc - displays
-	PORTC = 0b11111111;		// todos os pinos saídas
+	/* Pin 7 and 8 will be used as outputs so we can select which display we intend to use*/
+	DDRD = 0b11000000;			
+	/*In this couple of lines we initialize PORTC as output and make sure that all the displays are turned off*/
+	DDRC = 0b11111111;
 	screen3 = display[11];
 	screen2 = display[11];
 	screen1 = display[11];
-	
-	DDRB = 0b11100000;    // portb motor
+	/*PortB is the one used for the motor and is initialized in this 2 lines*/
+	DDRB = 0b11100000;
 	PORTB = 0b00100000;
 	
-	//interrupção externa
+	/*External interrupts are initialized here which will be used to determine which of the switches were pressed*/
 	EICRA = 0b10101010;
 	EIMSK = 0b00001111;
-	
-	OCR0 = 77;              // configura TC0 @16MHz
-	TCCR0 = 0b00001111;     // 5ms, prescaler 1024, CTC
-	TIMSK |= 0b00000010;    // interrupção TC0
-	
+	/*Here we initialize the Timer/Counter 0 with a prescaler of 1024 giving us a time of 5ms*/
+	OCR0 = 77;
+	TCCR0 = 0b00001111;    
+	TIMSK |= 0b00000010;    
+	/*OCR2 is where we give the microcontroler the information of the speed we want it to operate in, we forced it to start turned off,
+	this uses the Timer/Counter 2 so we also initialized it TC2 witch a prescaler of 64 mode 1 and phase correct*/
 	OCR2 = DC(0);
-	TCCR2 = 0b01100011;		//  TC2 - prescaler 64, modo 1, phase correct
+	TCCR2 = 0b01100011;
 	
-	sei();           //ativa flag I do SREG
+	sei();
 
 }
-
+/*Here we tell the program that there are functions on the bottom that will be needed*/
 void mudar_rot(void);
 void display_(void);
 
-// rotina interrupção TC0
+/*This is the interrupt functions starting with the Timer/Counter 0 and followed by the switches*/
 ISR(TIMER0_COMP_vect)
 {
 	display_();
 	flag = 1;
 }
 
-ISR(INT0_vect)  // switch_1
+ISR(INT0_vect) 
 {
 	switch_=1;
 }
 
-ISR(INT1_vect)  // switch_2
+ISR(INT1_vect) 
 {
 	switch_=2;
 }
 
-ISR(INT2_vect)	// switch_3
+ISR(INT2_vect)
 {
 	switch_=3;
 }
 
-ISR(INT3_vect)	// switch_4
+ISR(INT3_vect)
 {
 	switch_=4;
 }
 
-
-
-// rotina principal
+/*This is our main function where there is a case/switch that will operate acording to the switches that are pressed*/
 int main(void)
 {
 	inic();
@@ -87,44 +85,61 @@ int main(void)
 	{
 		switch(switch_)
 		{
-			case 1:				// switch_1 - 25% da velocidade nominal
+			/*Switch 1 will make the motor rotate at 25% of its nominal speed, and save the value "25" in variables
+			 so it is displayed in the 7 segment display*/
+			case 1:	
 			screen3 = display[5];
 			screen2 = display[2];
+			if(negative == 1)
+			{
+				screen1 = display[10];
+			}
 			motor = 25;
 			break;
 			
-			case 2:				// switch_2 - 70% da velocidade nominal
+			/*Switch 2 will make the motor rotate at 25% of its nominal speed, and save the value "25" in variables
+			 so it is displayed in the 7 segment display*/
+			case 2:	
 			screen3 = display[0];
 			screen2 = display[7];
+			if(negative == 1)
+			{
+				screen1 = display[10];
+			}
 			motor = 70;
 			break;
 			
-			
-			case 3:				// switch_3 - inverte o sentido de rotação
-			if(flag==0) {break;}
+			/*Switch 3 will check what value is being currently displayed ( "-" or " " ) and call the function that will make the motor
+			switch the direction of its rotation*/
+			case 3:	
+			if(flag==0) break;
 			flag = 0;
-			if(screen1 == display[11])
+			if(screen1 == display[11] && negative == 0)
 			{
 				screen1 = display[10];
+				negative = 1;
 			}else
 			{
 				screen1 = display[11];
+				negative = 0;
 			}
 			mudar_rot();
 			break;
-
-			case 4:			// switch_4 - para o motor
+			/*Switch 4 will make the motor stop and turn of the display*/
+			case 4:	
 			screen3 = display[11];
 			screen2 = display[11];
 			screen1 = display[11];
 			motor = 0;
 			break;
 		}
+		/*Resets the value of the switch*/
 		switch_=0;
+		/*Changes the motor speed*/
 		OCR2 = DC(motor);
 	}
 }
-
+/*In this function the direction of the motor will be checked and changed */
 void mudar_rot(void)
 {
 	if (PORTB & (1<<5))
@@ -140,7 +155,7 @@ void mudar_rot(void)
 	}
 }
 
-
+/*In this function we update the display with the values of the variables given from the above functions*/
 void display_(void)
 {
 	PORTD = 0b11000000;
@@ -150,8 +165,10 @@ void display_(void)
 	PORTC = screen2;
 	_delay_ms(1);
 	PORTD = 0b01000000;
+	/*This "if" condition is used to make sure the "-" sign wont appear when the motor is off*/
+	if(motor!=0){
 	PORTC = screen1;
-	_delay_ms(1);
+	_delay_ms(1);}
 }
 
 
